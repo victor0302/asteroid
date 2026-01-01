e5963796bf326f148ff99569da193cd6ead6dd54
Vic <victorsalazar.01.vv@gmail.com>
2026-01-01 04:09:02 -0700

    Player Movement

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
+        dt = clock.tick(60) / 1000      
+        player.update(dt)               
+        screen.fill("black")            
+        player.draw(screen)             
+        pygame.display.flip()           
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
--- player.py
+++ player.py
@@ -1,12 +1,23 @@
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, PLAYER_TURN_SPEED
 import pygame
 
 class Player (CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
 
 
