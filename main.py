84ae58fd0d6aa94841f602d8fafaf2feda92ec19
Vic <victorsalazar.01.vv@gmail.com>
2026-01-01 12:43:59 -0700

    Debugging player movement

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000      
-        player.update(dt)               
-        screen.fill("black")            
-        player.draw(screen)             
-        pygame.display.flip()           
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000         
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
-from constants import LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player (CircleShape):
@@ -18,6 +18,17 @@ class Player (CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        
+
+    def move(self,dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
 
 
 
@@ -32,7 +43,7 @@ def triangle(self):
 
 
 def draw(self, screen):
-        pygame.draw.polygon(screen,"white", self.triangle(),LINE_WIDTH)
+    pygame.draw.circle(screen, "red", self.position, 20)
 
 def update(self,dt):
     pass
