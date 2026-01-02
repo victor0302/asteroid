37f0ecd703d919acb7a76fad9845c9cbb7d28a73
Vic <victorsalazar.01.vv@gmail.com>
2026-01-01 22:44:07 -0700

    Added group class

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player = Player((SCREEN_WIDTH)/2,(SCREEN_HEIGHT)/2)
     while True:
         log_state()
@@ -16,9 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000      
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
      
 
