0f1b3b1ea1a136bbe5a6d75832423c87c44dc0e2
Vic <victorsalazar.01.vv@gmail.com>
2026-01-04 23:26:38 -0700

    Fixed shots group:

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    shot = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
-    Shot.containers = (shot,updatable,drawable)
+    Shot.containers = (shots,updatable,drawable)
     asteroid_field = AsteroidField()
     player = Player((SCREEN_WIDTH)/2,(SCREEN_HEIGHT)/2)
 
